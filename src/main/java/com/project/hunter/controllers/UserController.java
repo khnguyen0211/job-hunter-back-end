443e13714807b1829ea4268b763da99d4c218b08
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-10-16 16:13:37 +0700

    add response entity for api

--- src/main/java/com/project/hunter/controllers/UserController.java
+++ src/main/java/com/project/hunter/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.project.hunter.controllers;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +18,8 @@ import com.project.hunter.domain.dto.UserDto;
 import com.project.hunter.domain.entities.UserEntity;
 import com.project.hunter.services.UserService;
 
-
 @RestController()
-@RequestMapping("users") 
+@RequestMapping("users")
 public class UserController {
 
     private final UserService userService;
@@ -29,27 +29,32 @@ public class UserController {
     }
 
     @PostMapping()
-    public UserDto createUserAPI(@RequestBody UserEntity userEntity) {
-        return this.userService.handleSaveUser(userEntity);
+    public ResponseEntity<UserDto> createUserAPI(@RequestBody UserEntity userEntity) {
+        UserDto userDto = this.userService.handleSaveUser(userEntity);
+        return ResponseEntity.status(201).body(userDto);
     }
 
     @GetMapping()
-    public List<UserDto> getAllUsersApi() {
-        return this.userService.handleGetAllUser();
+    public ResponseEntity<List<UserDto>> getAllUsersApi() {
+        List<UserDto> userDtoList = this.userService.handleGetAllUser();
+        return ResponseEntity.status(200).body(userDtoList);
     }
 
     @GetMapping("/{id}")
-    public UserDto getUserDetailsApi(@PathVariable("id") UUID id) {
-        return this.userService.handleGetUserById(id);
+    public ResponseEntity<UserDto> getUserDetailsApi(@PathVariable("id") UUID id) {
+        UserDto userDto = this.userService.handleGetUserById(id);
+        return ResponseEntity.status(200).body(userDto);
     }
 
     @PutMapping("/{id}")
-    public UserDto updateUserApi(@PathVariable("id") UUID id, @RequestBody UpdateUserDto updateUserDto) {
-        return this.userService.handleUpdateUserById(id, updateUserDto);
+    public ResponseEntity<UserDto> updateUserApi(@PathVariable("id") UUID id, @RequestBody UpdateUserDto updateUserDto) {
+        UserDto userDto = this.userService.handleUpdateUserById(id, updateUserDto);
+        return ResponseEntity.status(200).body(userDto);
     }
 
     @DeleteMapping("/{id}")
-    public UserDto deleteUserApi(@PathVariable("id") UUID id) {
-        return this.userService.handleDeleteUserById(id);
+    public ResponseEntity<UserDto> deleteUserApi(@PathVariable("id") UUID id) {
+        UserDto userDto = this.userService.handleDeleteUserById(id);
+        return ResponseEntity.status(200).body(userDto);
     }
 }
