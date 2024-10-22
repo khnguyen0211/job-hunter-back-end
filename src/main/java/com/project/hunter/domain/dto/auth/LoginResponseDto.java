8ff682b58581db4813df7aeee3af5903201f9e83
Khoi Nguyen <pnknguyen0211@gmail.com>
2024-10-22 12:11:36 +0700

    fix

--- src/main/java/com/project/hunter/domain/dto/auth/LoginResponseDto.java
+++ src/main/java/com/project/hunter/domain/dto/auth/LoginResponseDto.java
@@ -1,10 +1,12 @@
 package com.project.hunter.domain.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.project.hunter.domain.dto.users.UserDto;
 
 
 public class LoginResponseDto {
     private UserDto user;
+    @JsonProperty("access_token")
     private String accessToken;
 
     public LoginResponseDto(UserDto user, String accessToken) {
